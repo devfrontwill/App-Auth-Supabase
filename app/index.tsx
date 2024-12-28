ebb0f290b6e60b44ef918e3c472cd2fc938206b5
Wiliam Roza <devfrontwill@gmail.com>
2024-12-28 16:32:51 -0300

    Feat: Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Login() {
+    return (
+        <View>
+            <Text>Página LOGIN</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    
+})
